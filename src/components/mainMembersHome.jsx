d8fa9c78749037ebb75a405ecd828ff05e272031
shivam <shivamthalwal@gmail.com>
2022-04-09 23:13:53 +0530

    contactForm Done

--- src/components/mainMembersHome.jsx
+++ src/components/mainMembersHome.jsx
@@ -3,13 +3,13 @@ import './components.css'
 function MainMembersHome () {
     return(
     <>
-      <div class="row">
+      <div className="row">
       <div className="col-md-4">
-      <div class="cardMain">
-        <div class="imgBx">
+      <div className="cardMain">
+        <div className="imgBx">
             <img src="https://images.unsplash.com/photo-1532123675048-773bd75df1b4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="images" />
         </div>
-        <div class="details">
+        <div className="details">
             <h2>SomeOne Famous<br></br><span>Director</span></h2>
         </div>
         <div className="position">
@@ -19,11 +19,11 @@ function MainMembersHome () {
       </div>
 
       <div className="col-md-4">
-       <div class="cardMain">
-         <div class="imgBx">
+       <div className="cardMain">
+         <div className="imgBx">
             <img src="https://images.unsplash.com/photo-1549417229-aa67d3263c09?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="images" />
          </div>
-         <div class="details">
+         <div className="details">
             <h2>SomeOne Famous<br></br><span>Producer</span></h2>
           </div>
           <div className="position">
@@ -33,11 +33,11 @@ function MainMembersHome () {
 </div>
 
 <div className="col-md-4">
-       <div class="cardMain">
-         <div class="imgBx">
+       <div className="cardMain">
+         <div className="imgBx">
             <img src="https://images.unsplash.com/photo-1548094878-84ced0f6896d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="images" />
          </div>
-         <div class="details">
+         <div className="details">
             <h2>SomeOne Famous<br></br><span>Actor</span></h2>
           </div>
         <div className="position">
