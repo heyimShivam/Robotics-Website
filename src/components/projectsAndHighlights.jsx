22a0c8bcb2dc665e540932d7db4d936177d5ca30
Arjun Thakur <21bme057@nith.ac.in>
2022-04-10 00:31:30 +0530

    projects 2

--- src/components/projectsAndHighlights.jsx
+++ src/components/projectsAndHighlights.jsx
@@ -1,140 +1,118 @@
 function ProjectHighLights() {
-    return(
-        <>
+  return (
+    <>
+      <div class="project-head">
+        <h1 class="activity-head">Project Highlights</h1>
+      </div>
       <div className="container">
-        <div >
+        <div>
           <section className="light">
-              <article className="postcard light blue">
-                <a className="postcard__img_link" href="#empty">
-                  <img
-                    className="postcard__img"
-                    src="https://picsum.photos/1000/1000"
-                    alt="pic Title"
-                  />
-                </a>
-                <div className="postcard__text t-dark">
-                  <h1 className="postcard__title blue">
-                    <a href="#empty">Podcast Title</a>
-                  </h1>
-                  {/* <div className="postcard__subtitle small">
+            <article className="postcard light blue">
+              <a className="postcard__img_link" href="#empty">
+                <img
+                  className="postcard__img"
+                  src="images/projects/humanoid.jpeg"
+                  alt="pic Title"
+                />
+              </a>
+              <div className="postcard__text t-dark">
+                <h1 className="postcard__title blue">
+                  <a href="https://robosocnith.com/index.html">
+                    Humanoid Robot
+                  </a>
+                </h1>
+                {/* <div className="postcard__subtitle small">*/}
+                <div className="postcard__bar"></div>
+                <div className="postcard__preview-txt">
+                  A humanoid robot is a robot with its body shape built to
+                  resemble the human body. The design may be for functional
+                  purposes, such as interacting with human tools and
+                  environments, for experimental purposes, such as the study of
+                  bipedal locomotion or for other purposes
+                </div>
+                <div>
+                  <button className="btn btn-outline-dark btn-small">
+                    <a href="https://robosocnith.com/index.html">
+                      <i className="fas fa-tag mr-2"></i>Learn More
+                    </a>
+                  </button>
+                </div>
+              </div>
+            </article>
+            <article className="postcard light red">
+              <a className="postcard__img_link" href="#empty">
+                <img
+                  className="postcard__img"
+                  src="images/projects/wheelchair.jpeg"
+                  alt="pic Title"
+                />
+              </a>
+              <div className="postcard__text t-dark">
+                <h1 className="postcard__title red">
+                  <a href="#empty">Brain Controlled Wheelchair</a>
+                </h1>
+                {/* <div className="postcard__subtitle small">
                     <time datetime="2020-05-25 12:00:00">
                       <i className="fas fa-calendar-alt mr-2"></i>Mon, May 25th
                       2020
                     </time>
                   </div> */}
-                  <div className="postcard__bar"></div>
-                  <div className="postcard__preview-txt">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Eligendi, fugiat asperiores inventore beatae accusamus odit
-                    minima enim, commodi quia, doloribus eius! Ducimus nemo
-                    accusantium maiores velit corrupti tempora reiciendis
-                    molestiae repellat vero. Eveniet ipsam adipisci illo iusto
-                    quibusdam, sunt neque nulla unde ipsum dolores nobis enim
-                    quidem excepturi, illum quos!
-                  </div>
-                  <ul className="postcard__tagbox">
-                  <li className="tag__item play blue">
-                      <a href="#empty">
-                        <i className="fas fa-tag mr-2"></i>Register
-                      </a>
-                    </li>
-                    <li className="tag__item play blue">
-                      <a href="#empty">
-                        <i className="fas fa-play mr-2"></i>Play Episode
-                      </a>
-                    </li>
-                  </ul>
+                <div className="postcard__bar"></div>
+                <div className="postcard__preview-txt">
+                  Brain Controlled Wheelchair uses Electroencephalogram (EEG)
+                  band worn on the user’s forehead which detects neural impulses
+                  that reach the scalp allowing the micro-controller on board to
+                  detect user’s thought process, interpret it and control
+                  wheelchair movements.
                 </div>
-              </article>
-              <article className="postcard light red">
-                <a className="postcard__img_link" href="#empty">
-                  <img
-                    className="postcard__img"
-                    src="https://picsum.photos/501/500"
-                    alt="pic Title"
-                  />
-                </a>
-                <div className="postcard__text t-dark">
-                  <h1 className="postcard__title red">
-                    <a href="#empty">Podcast Title</a>
-                  </h1>
-                  {/* <div className="postcard__subtitle small">
-                    <time datetime="2020-05-25 12:00:00">
+                <div>
+                  <button className="btn btn-outline-dark btn-small">
+                    <a href="https://robosocnith.com/index.html">
+                      <i className="fas fa-tag mr-2"></i>Learn More
+                    </a>
+                  </button>
+                  </div>
+              </div>
+            </article>
+            <article className="postcard light green">
+              <a className="postcard__img_link" href="#empty">
+                <img
+                  className="postcard__img"
+                  src="images/projects/balancingbot.jpeg"
+                  alt="pic Title"
+                />
+              </a>
+              <div className="postcard__text t-dark">
+                <h1 className="postcard__title green">
+                  <a href="#empty">Self-Balancing Bot</a>
+                </h1>
+                {/* <div className="postcard__subtitle small">    <time datetime="2020-05-25 12:00:00">
                       <i className="fas fa-calendar-alt mr-2"></i>Mon, May 25th
                       2020
                     </time>
                   </div> */}
-                  <div className="postcard__bar"></div>
-                  <div className="postcard__preview-txt">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Eligendi, fugiat asperiores inventore beatae accusamus odit
-                    minima enim, commodi quia, doloribus eius! Ducimus nemo
-                    accusantium maiores velit corrupti tempora reiciendis
-                    molestiae repellat vero. Eveniet ipsam adipisci illo iusto
-                    quibusdam, sunt neque nulla unde ipsum dolores nobis enim
-                    quidem excepturi, illum quos!
-                  </div>
-                  <ul className="postcard__tagbox">
-                  <li className="tag__item play red">
-                      <a href="#empty">
-                        <i className="fas fa-tag mr-2"></i>Register
-                      </a>
-                    </li>
-                    <li className="tag__item play red">
-                      <a href="#empty">
-                        <i className="fas fa-play mr-2"></i>Play Episode
-                      </a>
-                    </li>
-                  </ul>
+                <div className="postcard__bar"></div>
+                <div className="postcard__preview-txt">
+                  A self-balancing robot is a classic
+                  inverted pendulum control system that is solvable in two
+                  degrees of freedom for the simplest models. The bot attempts
+                  to correct for an induced lean angle by moving forward or
+                  backward and the goal is to return itself to vertical.
                 </div>
-              </article>
-              <article className="postcard light green">
-                <a className="postcard__img_link" href="#empty">
-                  <img
-                    className="postcard__img"
-                    src="https://picsum.photos/500/501"
-                    alt="pic Title"
-                  />
-                </a>
-                <div className="postcard__text t-dark">
-                  <h1 className="postcard__title green">
-                    <a href="#empty">Podcast Title</a>
-                  </h1>
-                  {/* <div className="postcard__subtitle small">
-                    <time datetime="2020-05-25 12:00:00">
-                      <i className="fas fa-calendar-alt mr-2"></i>Mon, May 25th
-                      2020
-                    </time>
-                  </div> */}
-                  <div className="postcard__bar"></div>
-                  <div className="postcard__preview-txt">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Eligendi, fugiat asperiores inventore beatae accusamus odit
-                    minima enim, commodi quia, doloribus eius! Ducimus nemo
-                    accusantium maiores velit corrupti tempora reiciendis
-                    molestiae repellat vero. Eveniet ipsam adipisci illo iusto
-                    quibusdam, sunt neque nulla unde ipsum dolores nobis enim
-                    quidem excepturi, illum quos!
+                <div>
+                  <button className="btn btn-outline-dark btn-small">
+                    <a href="https://robosocnith.com/index.html">
+                      <i className="fas fa-tag mr-2"></i>Learn More
+                    </a>
+                  </button>
                   </div>
-                  <ul className="postcard__tagbox">
-                  <li className="tag__item play green">
-                      <a href="#empty">
-                        <i className="fas fa-tag mr-2"></i>Register
-                      </a>
-                    </li>
-                    <li className="tag__item play green">
-                      <a href="#empty">
-                        <i className="fas fa-play mr-2"></i>Play Episode
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </article>
+              </div>
+            </article>
           </section>
         </div>
       </div>
     </>
-    )
+  );
 }
 
 export default ProjectHighLights;
