aadc0b8184d4bc28f65ed4e85e12e9f9097a3bcd
Milind Gupta <21bee011@nith.ac.in>
2022-06-19 17:13:27 +0530

    achievementpage

---
+++ src/pages/achievements.jsx
@@ -0,0 +1,18 @@
+import "./pages.css"
+import Achievements3 from "../components/Achievements";
+
+
+function Achievements()
+ {
+  return (
+    <>
+
+<Achievements3/>
+
+  </>
+  );
+    
+
+}
+
+export default Achievements;
--- src/routing/routing.jsx
+++ src/routing/routing.jsx
@@ -3,13 +3,14 @@ import Footer from '../components/footer';
 import Home from '../pages/home';
 import Gallery from '../pages/gallery'
 import Members from '../pages/members';
+import Achievements from '../pages/Achievements';
 
 function Routing() {
     return (    
       <Routes>
         <Route exact path='/' element={< Home />}></Route>
         <Route exact path='/about' element={< Footer />}></Route>
-        <Route exact path='/contact' element={< Gallery />}></Route>
+        <Route exact path='/Achievements' element={< Achievements />}></Route>
         <Route exact path='/contact' element={< Gallery />}></Route>
         <Route exact path='/gallery' element={< Gallery />}></Route>
         <Route exact path='/members' element={< Members />}></Route>
