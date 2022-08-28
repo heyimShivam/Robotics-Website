b82800e9c67814528254ca9cbd6e967f6973e4dd
Arjun Thakur <21bme057@nith.ac.in>
2022-08-28 15:35:37 +0530

    contact form made responsive again

--- src/components/mainMembersHome.jsx
+++ src/components/mainMembersHome.jsx
@@ -1,55 +1,60 @@
-import './components.css'
+/** @format */
 
-function MainMembersHome () {
-    return(
+import "./components.css";
+
+function MainMembersHome() {
+  return (
     <>
-      <div className="row">
-      <div className="col-md-4">
-      <div class="cardMain">
-        <div class="imgBx">
-            <img src="members\kashish sir.jpeg" />
+      <div className='row'>
+        <div className='col-lg-4 col-md-12 my-4'>
+          <div class='cardMain'>
+            <div class='imgBx'>
+              <img src='members\sant ram sir.jpeg' />
+            </div>
+            <div class='details'>
+              <h2>
+                Dr. Sant Ram Chauhan<br></br>
+                <span>Asst. Professor Mechanical </span>
+              </h2>
+            </div>
+            <div className='position'>
+              Faculty Coordinator
+            </div>
+          </div>
         </div>
-        <div class="details">
-            <h2>Kashish Verma<br></br><span></span></h2>
+
+        <div className='col-lg-4 col-md-6 my-4'>
+          <div class='cardMain'>
+            <div class='imgBx'>
+              <img src='members\kashish sir.jpeg' />
+            </div>
+            <div class='details'>
+              <h2>
+                Kashish Verma<br></br>
+                <span></span>
+              </h2>
+            </div>
+            <div className='position'>Co-Founder</div>
+          </div>
         </div>
-        <div className="position">
-        Co-Founder
+
+        <div className='col-lg-4 col-md-6 my-4'>
+          <div class='cardMain'>
+            <div class='imgBx'>
+              <img src='members\lamy sir.jpeg' />
+            </div>
+            <div class='details'>
+              <h2>
+                Late Lamyanba Heisnam<br></br>
+                <span></span>
+              </h2>
+            </div>
+            <div className='position'>Co-Founder</div>
+          </div>
         </div>
       </div>
-      </div>
-
-      <div className="col-md-4">
-       <div class="cardMain">
-         <div class="imgBx">
-            <img src="members\sant ram sir.jpeg" />
-         </div>
-         <div class="details">
-            <h2>Dr. Sant Ram Chauhan<br></br><span>Asst. Professor
-Mechanical </span></h2>
-          </div>
-          <div className="position">
-          Faculty Coordinator
-        </div>
-       </div>
-</div>
-
-<div className="col-md-4">
-       <div class="cardMain">
-         <div class="imgBx">
-            <img src="members\lamy sir.jpeg" />
-         </div>
-         <div class="details">
-            <h2>Late Lamyanba Heisnam<br></br><span></span></h2>
-          </div>
-        <div className="position">
-        Co-Founder
-        </div>
-       </div>
-       </div>
-
-       </div>
     </>
-    )
+  );
 }
 
 export default MainMembersHome;
