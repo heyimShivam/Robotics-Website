275ad819ac18bc8930ae023e6377ccbe29bd6655
upstream <21bee011@nith.ac.in>
2022-09-25 23:48:42 +0530

    homepage...

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -7,11 +7,11 @@ import MainMembersHome from "../components/mainMembersHome";
 import ProjectHighLights from "../components/projectsAndHighlights";
 import ContactUs from "../components/contactUs";
 import Highlights from "../components/highlights";
-import LOGO1 from './../images/png/linkedin-ico.png';
-import LOGO2 from './../images/png/fb-ico.png';
-import LOGO3 from './../images/png/github-ico.png';
-import LOGO4 from './../images/png/insta-ico.png';
-import LOGO5 from './../images/png/yt-ico.png';
+import LOGO1 from "./../images/png/linkedin-ico.png";
+import LOGO2 from "./../images/png/fb-ico.png";
+import LOGO3 from "./../images/png/github-ico.png";
+import LOGO4 from "./../images/png/insta-ico.png";
+import LOGO5 from "./../images/png/yt-ico.png";
 
 function RoboticsText() {
   const styles = useSpring({
@@ -44,88 +44,89 @@ function Home() {
         </div>
 
         {/* This Div block is for the text present on Home Page */}
-        <div className="container-fluid main-element d-flex justify-content-center" >
-        <div className="roboticsname">
-
-        <div class='home-hero__socials'>
-            <div class='home-hero__social'>
-              <a href='https://www.linkedin.com/school/robotics-society-national-institute-of-technology-hamipur/about/' class='home-hero__social-icon-link'>
-                <img src={LOGO1} alt='icon' class='home-hero__social-icon' />
-              </a>
-            </div>
-            <div class='home-hero__social'>
-              <a href='https://www.facebook.com/robo.soc.nith/' class='home-hero__social-icon-link text-light'>
-                <img src={LOGO2} alt='icon' class='home-hero__social-icon' />
-              </a>
-            </div>
-            <div class='home-hero__social'>
-              <a href='https://www.instagram.com/robosocnith/?hl=en' class='home-hero__social-icon-link'>
-                <img src={LOGO4} alt='icon' class='home-hero__social-icon' />
-              </a>
-            </div>
-            <div class='home-hero__social'>
-              <a href='https://www.youtube.com/channel/UCcNk3wOBYsFvdoYoHiyhtng' class='home-hero__social-icon-link'>
-                <img src={LOGO5} alt='icon' class='home-hero__social-icon' />
-              </a>
-            </div>
-            <div class='home-hero__social'>
-              <a href='https://github.com/robosocnith/robosocnith.github.io' class='home-hero__social-icon-link home-hero__social-icon-link--bd-none'>
-                <img src={LOGO3} alt='icon' class='home-hero__social-icon' />
-              </a>
-            </div>
-        </div>
-
-          <div className="row">
-            <div className="col">
-            <svg width="100%" viewBox="0 0 1320 300">
-                <text x="50%" y="50%" dy=".35em" text-anchor="middle" >
-                  Robotics Society
-                </text>
-            </svg>
-            </div>
-
-            <div className="row text-light">
-              <div className="col robosoc-text-header">
-                To be a luminary of the indian society in the field of Robotics. 
+        <div className='container-fluid main-element d-flex justify-content-center'>
+          <div className='roboticsname'>
+            <div class='home-hero__socials'>
+              <div class='home-hero__social'>
+                <a href='https://www.linkedin.com/school/robotics-society-national-institute-of-technology-hamipur/about/' class='home-hero__social-icon-link'>
+                  <img src={LOGO1} alt='icon' class='home-hero__social-icon' />
+                </a>
+              </div>
+              <div class='home-hero__social'>
+                <a href='https://www.facebook.com/robo.soc.nith/' class='home-hero__social-icon-link text-light'>
+                  <img src={LOGO2} alt='icon' class='home-hero__social-icon' />
+                </a>
+              </div>
+              <div class='home-hero__social'>
+                <a href='https://www.instagram.com/robosocnith/?hl=en' class='home-hero__social-icon-link'>
+                  <img src={LOGO4} alt='icon' class='home-hero__social-icon' />
+                </a>
+              </div>
+              <div class='home-hero__social'>
+                <a href='https://www.youtube.com/channel/UCcNk3wOBYsFvdoYoHiyhtng' class='home-hero__social-icon-link'>
+                  <img src={LOGO5} alt='icon' class='home-hero__social-icon' />
+                </a>
+              </div>
+              <div class='home-hero__social'>
+                <a href='https://github.com/robosocnith/robosocnith.github.io' class='home-hero__social-icon-link home-hero__social-icon-link--bd-none'>
+                  <img src={LOGO3} alt='icon' class='home-hero__social-icon' />
+                </a>
               </div>
             </div>
 
-            <div className="row text-light">
-              <div className="col robosoc-text-header">
-                Build and sustain a culture to be self reliant to accomplish our vision, by emphasizing on the development of individual quality.
+            <div className='row'>
+              <div className='col'>
+                <svg width='100%' viewBox='0 0 1320 300'>
+                  <text x='50%' y='50%' dy='.35em' text-anchor='middle'>
+                    Robotics Society
+                  </text>
+                </svg>
               </div>
-            </div>
 
-            <div className="row robosoc-text-header-box">
-              <div className="col robosoc-text-header">
-                <button className="header-button btn btn-secondary">Shivam</button>
+              <div className="row robosoc-text-header-box">
+               
+                <div className="col robosoc-text-header">
+          
+                  <div className='row text-light'>
+                    <div className='col robosoc-text-header'>To be a luminary of the indian society in the field of Robotics.</div>
+                  </div>
+                </div>
               </div>
-              <div className="col robosoc-text-header">
-                <button className="header-button btn btn-secondary">Shivam</button>
+                <div className='row text-light'>
+                  <div className='col robosoc-text-header'>Build and sustain a culture to be self reliant to accomplish our vision, by emphasizing on the development of individual quality.</div>
+                </div>
+
+                <div className='row robosoc-text-header-box'>
+                  <div className='col robosoc-text-header'>
+                    <button className='header-button btn btn-secondary'>Shivam</button>
+                  </div>
+                  <div className='col robosoc-text-header'>
+                    <button className='header-button btn btn-secondary'>Shivam</button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
+          <div class='home-hero__mouse-scroll-cont'>
+            <div class='mouse'></div>
+          </div>
         </div>
+        <div className="container">
+          <MainMembersHome />
         </div>
-        
-
-      </div>
-      <div className="container">
-        <MainMembersHome />
-      </div>
-      <div>
-        <ProjectHighLights />
-      </div>
-      <div>
-        <Highlights />
-      </div>
-      <div >
-        <ContactUs />
-        <br />
-        <br />
-      </div>
-    </>
-  );
+        <div>
+          <ProjectHighLights />
+        </div>
+        <div>
+          <Highlights />
+        </div>
+        <div >
+          <ContactUs />
+          <br />
+          <br />
+        </div>
+      </>
+      );
 }
 
-export default Home;
+      export default Home;
