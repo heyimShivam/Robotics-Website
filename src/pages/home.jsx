116a53c1088801ef3a23783bd19a713965fb0183
Arjun Thakur <21bme057@nith.ac.in>
2022-11-04 16:00:54 +0530

    navbar modified

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -235,9 +235,7 @@ function Home() {
         </div>
 
         {/* This Div block is for the text present on Home Page */}
-        <div className='container-fluid main-element d-flex justify-content-center'>
-          <div className='roboticsname'>
-            <div class='home-hero__socials'>
+        <div class='home-hero__socials'>
               <div class='home-hero__social'>
                 <a href='https://www.linkedin.com/school/robotics-society-national-institute-of-technology-hamipur/about/' class='home-hero__social-icon-link'>
                   <img src={LOGO1} alt='icon' class='home-hero__social-icon' />
@@ -264,6 +262,10 @@ function Home() {
                 </a>
               </div>
             </div>
+            
+        <div className='container-fluid main-element d-flex justify-content-center'>
+          <div className='roboticsname'>
+            
 
             <div className='row'>
               <div className='col main__page__heading'>
